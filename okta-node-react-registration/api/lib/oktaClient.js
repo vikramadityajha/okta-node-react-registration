9f3761a98464dbca8e96ae517f281ab75f025c22
Vikramaditya Jha <108vikramadityajha@gmail.com>
2022-01-12 15:07:40 +0530

    added .gitignore file

--- okta-node-react-registration/api/lib/oktaClient.js
+++ okta-node-react-registration/api/lib/oktaClient.js
@@ -1,8 +1,8 @@
 const okta = require('@okta/okta-sdk-nodejs');
 
 const client = new okta.Client({
-  orgUrl: 'https://dev-31166667.okta.com',
-  token: '00RRi1xq3rTdO_cE6pBX2_5zlWPmpedOlsAXKRvLSk'
+  orgUrl: 'https://{yourOktaDomain}',
+  token: '{APIToken}'
 });
 
 module.exports = client;
